f875b01dc988de9eead83cb6ffe04c61e65619e9
Dmytryi <muzalevskyid333@gmail.com>
2025-07-10 13:04:57 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("1.Посмотреть продукты");
+        Console.WriteLine("2.Посмотреть корзину");
+        Console.WriteLine("3.Способ оплаты");
+        Console.WriteLine("4.Получить чек");
+
+
+        Console.Write("\nВыберите действие: ");
+        string chooseOption = Console.ReadLine();
+
+        switch (chooseOption) {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+
+            default:
+                break;
+        }
+    }
+}
